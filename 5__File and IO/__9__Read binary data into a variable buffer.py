589cf155686220676f655da2dfdbe6b578f528fa
Qiu Yu <qy2011080020008@163.com>
2019-07-13 15:41:12 +0800

    add some content of Ch5

--- 5__File and IO/__9__Read binary data into a variable buffer.py
+++ 5__File and IO/__9__Read binary data into a variable buffer.py
@@ -27,3 +27,46 @@ if __name__ == "__main__":
 
     with open('newsample.bin', 'wb') as f:
         f.write(buf)
+
+    """
+        文件对象的 readinto() 方法能被用来为预先分配内存的数组填充数据，甚至包括由 array 模块或 numpy 库创建的数组。 
+        
+        和普通 read() 方法不同的是， readinto() 填充已存在的缓冲区而不是为新对象重新分配内存再返回它们。 
+        
+        因此，你可以使用它来避免大量的内存分配操作。 
+        
+        比如，如果你读取一个由相同大小的记录组成的二进制文件时，你可以像下面这样写：
+    """
+    separator_content()
+    record_size = 32  # Size of each record (adjust value)
+
+    buf = bytearray(record_size)
+    with open('somefile', 'rb') as f:
+        while True:
+            n = f.readinto(buf)
+            if n < record_size:
+                break
+            # Use the contents of buf
+            pass
+
+    """
+        另外有一个有趣特性就是 memoryview ， 它可以通过零复制的方式对已存在的缓冲区执行切片操作，甚至还能修改它的内容。
+    """
+    separator_content()
+
+    m1 = memoryview(buf)
+    m2 = m1[-5:]
+    print(m2)
+    m2[:] = b'WORLD'
+    print(buf)
+
+    """
+        使用 f.readinto() 时需要注意的是，你必须检查它的返回值，也就是实际读取的字节数。
+    
+        如果字节数小于缓冲区大小，表明数据被截断或者被破坏了(比如你期望每次读取指定数量的字节)。
+    
+        最后，留心观察其他函数库和模块中和 into 相关的函数(比如 recv_into() ， pack_into() 等)。
+     
+        Python的很多其他部分已经能支持直接的I/O或数据访问操作，这些操作可被用来填充或修改数组和缓冲区内容。
+    """
+
